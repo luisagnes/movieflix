8d1a01dc09f5fc327da18c86d686cf70be0e7a4f
luisagnes <lixilipe@gmail.com>
2023-06-22 01:28:10 -0300

    Criação página home#3

--- movieflix/src/components/Navbar.jsx
+++ movieflix/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
         <h1 className='text-yellow-600 text-4xl font-bold cursor pointer'>MOVIEFLIX</h1>
         <div>
-            <button className='text-white pr-4'>Sign In</button>
-            <button className='bg-yellow-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
+            <button className='text-white pr-4'>Entrar</button>
+            <button className='bg-yellow-600 px-4 py-2 rounded cursor-pointer text-white'>Inscrever-se</button>
         </div>
     </div>
   )
